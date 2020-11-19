043347de9b9a98fd9c9f1d7172db1497e01d24a8
Paweł Wojtuń <panpawelw@pm.me>
2020-11-20 00:29:31 +0100

    Add Spring Boot backend components

---
+++ src/main/java/com/panpawelw/eyetraining/EyeTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.panpawelw.eyetraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panpawelw/eyetraining/EyeTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panpawelw.eyetraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
