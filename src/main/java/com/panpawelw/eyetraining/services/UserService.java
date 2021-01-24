2fa37f802cdcc23d7b21ce73726fcd2ec23fad0d
Paweł Wojtuń <panpawelw@pm.me>
2021-01-24 18:39:09 +0100

    Add methods to UserService

--- src/main/java/com/panpawelw/eyetraining/services/UserService.java
+++ src/main/java/com/panpawelw/eyetraining/services/UserService.java
@@ -1,8 +1,12 @@
 package com.panpawelw.eyetraining.services;
 
+import com.panpawelw.eyetraining.entities.User;
 import com.panpawelw.eyetraining.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +16,14 @@ public class UserService {
   @Autowired
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+  public User findUserByUserName(String userName) {
+    Optional<User> user = userRepository.findUserByUserName(userName);
+    return user.orElse(null);
+  }
+
+  public void SaveUser(User user) throws DataIntegrityViolationException {
+    userRepository.saveAndFlush(user);
   }
 }
